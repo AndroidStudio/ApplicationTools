c58d999b5492d313c4eee36d99cd72d359e882c1
witchcraftstudios <developer@witchcraftstudios.com>
2016-04-19 15:12:35 +0200

    Update

--- app/src/main/java/com/android/tools/MainActivity.java
+++ app/src/main/java/com/android/tools/MainActivity.java
@@ -1,11 +1,8 @@
 package com.android.tools;
 
 import android.os.Bundle;
-import android.widget.Toast;
-
-import app.tools.manager.DownloadListener;
-import app.tools.manager.DownloadManager;
-import app.tools.manager.ErrorMessage;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends BaseActivity {
 
@@ -13,6 +10,15 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            private byte[] a = new byte[40 * 1024 * 1024];
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
 }
